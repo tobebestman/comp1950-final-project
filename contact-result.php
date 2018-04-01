44646b7cc708437957918073013a75a034f36b4f
jin gang li <tobebestman@gmail.com>
2018-03-31 21:42:00 -0700

    implement the contact us page.

---
+++ contact-result.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>contact result</title>
+</head>
+
+<body>
+    <h1>Here is data come from the html form:</h1>
+    Welcome
+    <?php echo $_POST["first-name"]?> ,
+    <?php echo $_POST["last-name"]?>.
+</body>
+
+</html>
